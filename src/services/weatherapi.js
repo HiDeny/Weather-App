d3c4996e2f55a3db10887273ea84b36d5128c5cc
HiDeny <114344945+HiDeny@users.noreply.github.com>
2023-08-27 18:29:46 +0200

    get weather data and filter weather data feature

---
+++ src/services/weatherapi.js
@@ -0,0 +1,44 @@
+const apiKey = 'key=0ef9234ffd8140e0bcf145942232508';
+
+const baseURL = 'https://api.weatherapi.com/v1';
+const currentWeather = '/current.json';
+// const forecast = '/forecast.json';
+const testLocation = 'Pringle Bay';
+
+const getWeatherData = async (location) => {
+  const completeURL = `${baseURL}${currentWeather}?${apiKey}&q=${location}`;
+  try {
+    const response = await fetch(completeURL);
+    const responseJSON = await response.json();
+
+    return responseJSON;
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const filterWeatherData = async (weatherData) => {
+  const locationData = await weatherData;
+  const { name, region, country } = locationData.location;
+
+  const { temp_c, temp_f, uv } = locationData.current;
+  const { feelslike_c, feelslike_f } = locationData.current;
+
+  const { wind_kph, wind_mph, wind_dir } = locationData.current;
+  const { gust_kph, gust_mph } = locationData.current;
+
+  const { text, icon } = locationData.current.condition;
+
+  const messageHead = `${name} - ${country}`;
+  const messageTemp = `Temp: ${temp_c}C Feels like: ${feelslike_c}C UV: ${uv}`;
+  const messageWind = `Wind: ${wind_kph}K/H Gust: ${gust_kph}K/H Direction: ${wind_dir}`;
+  const messageText = `It's ${text}`;
+
+  console.log(messageHead);
+  console.log(messageTemp);
+  console.log(messageWind);
+  console.log(messageText);
+};
+
+const weatherData = getWeatherData(testLocation);
+filterWeatherData(weatherData);
